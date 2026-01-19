e97d9abdd30844956f79ece4e5b2934b52ca4d27
Luna <mhaiplick1506@gmail.com>
2026-01-19 21:59:35 +0100

    wofi style.scss temporarly removed

--- home/luna/home.nix
+++ home/luna/home.nix
@@ -16,7 +16,7 @@
     ./modules/cursor.nix
     ./modules/hyprpaper.nix
     ./modules/theme.nix
-    ./modules/wofi.nix
+#    ./modules/wofi.nix
     ./modules/ghostty.nix
 #    ./modules/neovim.nix
   ];
--- home/luna/modules/wofi.nix
+++ home/luna/modules/wofi.nix
@@ -1,19 +1,8 @@
 { pkgs, ... }:
 
 {
-  programs.wofi = {
-    enable = true;
+  programs.wofi.enable = true;
 
-    settings = {
-      show = "drun";
-      allow_images = true;
-      gtk_dark = true;
-      width = 600;
-      height = 400;
-    };
-  };
-
-  # Wofi CSS direkt kopieren
   home.file.".config/wofi/style.css".text = pkgs.runCommand "compile-wofi-style" {
     buildInputs = [ pkgs.sass ];
   } ''
