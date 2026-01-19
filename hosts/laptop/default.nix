7022272adf52fc4fb8842fcae023c2f322fef0a6
Luna <mhaiplick1506@gmail.com>
2026-01-19 21:43:28 +0100

    bugfix

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -33,5 +33,7 @@
   # Wichtig: Dieses Feld sollte der NixOS-Version deiner ERSTinstallation entsprechen.
   # In deiner Konfiguration stand "25.11", aktuell existiert 24.11 stabil.
   system.stateVersion = "24.11";
+
+  nixpkgs.config.allowUnfree = true;
 }
 
