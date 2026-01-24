d38cef581f831fba8bc6d30469c78fc3ac8a23f5
Luna <mhaiplick1506@gmail.com>
2026-01-24 23:04:28 +0100

    new aliases

--- dotfiles/modules/aliases.nix
+++ dotfiles/modules/aliases.nix
@@ -3,8 +3,15 @@
 {
   environment.shellAliases = {
     # Passe den Pfad zu deinem Repo an, falls anders:
-    rebuild = "sudo nixos-rebuild switch --flake /home/${username}/nixos/dotfiles#nixos";
-    update = "cd /home/${username}/nixos/dotfiles && nix flake update && cd -";
+    rebuild = "sudo nixos-rebuild switch --flake ./dotfiles#nixos";
+    update = "cd ./dotfiles && nix flake update && cd -";
+    garbage = "sudo nix-collect-garbage";
+    clear_efi = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5";
+    clean = ''
+      sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5 &&
+      sudo nix-collect-garbage
+    '';
+    setup_tim = "./update-setup.sh --username timp --fullname 'Tim Pagels' --nvidia-alt true --dest ~/src/nixos/dotfiles --monitor 'HDMI-A-1' --zoom '1.5'";
   };
 }
 
