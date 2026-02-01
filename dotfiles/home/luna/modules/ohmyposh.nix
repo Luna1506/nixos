924385b5a3fab24fb4b203b1be5ac98e897e396e
Luna <mhaiplick1506@gmail.com>
2026-02-01 23:04:40 +0100

    ohmyposh edited

--- dotfiles/home/luna/modules/ohmyposh.nix
+++ dotfiles/home/luna/modules/ohmyposh.nix
@@ -3,15 +3,19 @@
   programs.oh-my-posh = {
     enable = true;
     package = pkgs.oh-my-posh;
-    # WICHTIG: keine automatische Bash-Integration, sonst lädt er evtl. ein anderes Theme
+
+    # WICHTIG: keine automatische Bash-Integration,
+    # sonst lädt evtl. ein Default-Theme mit Farben
     enableBashIntegration = false;
   };
 
+  # oh-my-posh Theme – komplett monochrom, KEINE backgrounds
   home.file.".config/ohmyposh/theme.json".text = ''
     {
       "$schema": "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json",
       "version": 2,
       "final_space": true,
+
       "blocks": [
         {
           "type": "prompt",
@@ -19,26 +23,30 @@
           "segments": [
             {
               "type": "session",
-              "style": "powerline",
-              "foreground": "#000000",
-              "background": "#ffffff",
-              "powerline_symbol": ""
+              "style": "plain",
+              "foreground": "#ffffff"
+            },
+            {
+              "type": "text",
+              "text": "➜",
+              "style": "plain",
+              "foreground": "#888888"
             },
             {
               "type": "path",
-              "style": "powerline",
-              "foreground": "#000000",
-              "background": "#ffffff",
-              "powerline_symbol": "",
-              "properties": { "style": "folder" }
+              "style": "plain",
+              "foreground": "#ffffff",
+              "properties": {
+                "style": "folder"
+              }
             },
             {
               "type": "git",
-              "style": "powerline",
-              "foreground": "#000000",
-              "background": "#ffffff",
-              "powerline_symbol": "",
-              "properties": { "branch_icon": " " }
+              "style": "plain",
+              "foreground": "#aaaaaa",
+              "properties": {
+                "branch_icon": " "
+              }
             }
           ]
         }
@@ -46,10 +54,9 @@
     }
   '';
 
+  # Bash lädt GENAU dieses Theme
   programs.bash = {
     enable = true;
-
-    # ganz am Ende initialisieren, damit nichts danach dein Prompt überschreibt
     initExtra = ''
       eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/theme.json)"
     '';
