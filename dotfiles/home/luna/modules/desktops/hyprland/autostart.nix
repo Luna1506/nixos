830f74cfd4e73675a64e17a55f84690714e7925d
Luna <mhaiplick1506@gmail.com>
2026-02-23 23:45:25 +0100

    2026-02-23 23:45:25

--- dotfiles/home/luna/modules/desktops/hyprland/autostart.nix
+++ dotfiles/home/luna/modules/desktops/hyprland/autostart.nix
@@ -1,10 +1,28 @@
 { ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    "exec-once" = [
-      "$terminal"
+
+    exec-once = [
+      # Workspace 1 aktivieren
+      "hyprctl dispatch workspace 1"
+
+      # Hauptterminal (links groß)
+      "ghostty --title main"
+
+      # Rechts oben
+      "ghostty --title matrix -e cmatrix"
+
+      # Rechts unten
+      "ghostty --title cava -e cava"
+
+      # Hintergrund
       "hyprpaper"
+    ];
+
+    windowrulev2 = [
+      "workspace 1, title:^(main)$"
+      "workspace 1, title:^(matrix)$"
+      "workspace 1, title:^(cava)$"
     ];
   };
 }
-
