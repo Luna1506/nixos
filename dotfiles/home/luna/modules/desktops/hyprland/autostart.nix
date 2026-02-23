62d1ed2152e20fc176c6b2acd513218a96d5f40f
Luna <mhaiplick1506@gmail.com>
2026-02-24 00:01:20 +0100

    2026-02-24 00:01:20

--- dotfiles/home/luna/modules/desktops/hyprland/autostart.nix
+++ dotfiles/home/luna/modules/desktops/hyprland/autostart.nix
@@ -3,11 +3,12 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "hyprpaper"
+      "$terminal"
 
       # Alles auf Workspace 1
-      "[workspace 1 silent] ghostty --title main"
-      "[workspace 1 silent] bash -lc 'sleep 0.2 && exec ghostty --title matrix -e cmatrix'"
-      "[workspace 1 silent] bash -lc 'sleep 0.4 && exec ghostty --title cava -e cava'"
+      # "[workspace 1 silent] ghostty --title main"
+      # "[workspace 1 silent] bash -lc 'sleep 0.2 && exec ghostty --title matrix -e cmatrix'"
+      # "[workspace 1 silent] bash -lc 'sleep 0.4 && exec ghostty --title cava -e cava'"
     ];
   };
 }
