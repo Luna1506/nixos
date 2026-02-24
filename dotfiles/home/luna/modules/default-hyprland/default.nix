14b2508a24af6feb0424f102eec313213d48bf4e
Luna <mhaiplick1506@gmail.com>
2026-02-24 18:42:17 +0100

    2026-02-24 18:42:17

--- dotfiles/home/luna/modules/default-hyprland/default.nix
+++ dotfiles/home/luna/modules/default-hyprland/default.nix
@@ -9,6 +9,14 @@
     ./input.nix
     ./binds.nix
     ./rules.nix
+    ./ghostty.nix
+    ./hyprlock.nix
+    ./hyprpaper.nix
+    ./nwg-dock.nix
+    ./starship.nix
+    ./theme.nix
+    ./waybar.nix
+    ./wofi.nix
   ];
 
   wayland.windowManager.hyprland.enable = true;
