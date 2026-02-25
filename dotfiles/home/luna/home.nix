9933e79c6599b9decf3ff47ee32ab9de1c798c3a
Luna <mhaiplick1506@gmail.com>
2026-02-25 20:02:02 +0100

    2026-02-25 20:02:02

--- dotfiles/home/luna/home.nix
+++ dotfiles/home/luna/home.nix
@@ -31,7 +31,7 @@
 
     # nach dem fakeSha256-run ersetzen:
     rev = "main"; # besser: commit hash pinnen
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-Y9VJTv62yR3rjIdZz1SJEL9ithL6CnFiTBU1zs8b6+U=";
   };
 }
 
