086f595167860ab39673a0066fea4fa1ec5f6497
Luna <mhaiplick1506@gmail.com>
2026-02-25 19:39:52 +0100

    2026-02-25 19:39:52

--- dotfiles/home/luna/modules/quickshell/quickshell-bar-dock.nix
+++ dotfiles/home/luna/modules/quickshell/quickshell-bar-dock.nix
@@ -3,13 +3,27 @@
 let
   cfg = config.programs.quickshellBarDock;
 
-  fileText = path: builtins.readFile path;
-
+  # Build a single config dir in the store containing all QML files
   configName = cfg.configName;
+
+  quickshellConfigDir =
+    pkgs.linkFarm "quickshell-${configName}" [
+      { name = "shell.qml"; path = ./shell.qml; }
+
+      { name = "components/Bar.qml"; path = ./components/Bar.qml; }
+      { name = "components/Dock.qml"; path = ./components/Dock.qml; }
+      { name = "components/GlassRect.qml"; path = ./components/GlassRect.qml; }
+      { name = "components/IconButton.qml"; path = ./components/IconButton.qml; }
+      { name = "components/WorkspaceSwitcher.qml"; path = ./components/WorkspaceSwitcher.qml; }
+      { name = "components/MprisMini.qml"; path = ./components/MprisMini.qml; }
+      { name = "components/BluetoothIndicator.qml"; path = ./components/BluetoothIndicator.qml; }
+      { name = "components/WifiIndicator.qml"; path = ./components/WifiIndicator.qml; }
+      { name = "components/Tray.qml"; path = ./components/Tray.qml; }
+    ];
 in
 {
   options.programs.quickshellBarDock = {
-    enable = lib.mkEnableOption "Quickshell bar + dock (wrapper around HM programs.quickshell)";
+    enable = lib.mkEnableOption "Quickshell bar + dock";
 
     package = lib.mkOption {
       type = lib.types.package;
@@ -39,35 +53,19 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = cfg.extraPackages;
 
-    # IMPORTANT: use the official HM quickshell module to bundle config into one store dir
     programs.quickshell = {
       enable = true;
       package = cfg.package;
 
       activeConfig = configName;
 
-      configs.${configName} = {
-        "shell.qml" = fileText ./shell.qml;
+      # IMPORTANT: configs.<name> is an ABSOLUTE PATH to a directory
+      configs.${configName} = quickshellConfigDir;
 
-        # Keep components in a real subdir; relative import works because HM bundles them together.
-        "components/Bar.qml" = fileText ./components/Bar.qml;
-        "components/Dock.qml" = fileText ./components/Dock.qml;
-        "components/GlassRect.qml" = fileText ./components/GlassRect.qml;
-        "components/IconButton.qml" = fileText ./components/IconButton.qml;
-        "components/WorkspaceSwitcher.qml" = fileText ./components/WorkspaceSwitcher.qml;
-        "components/MprisMini.qml" = fileText ./components/MprisMini.qml;
-        "components/BluetoothIndicator.qml" = fileText ./components/BluetoothIndicator.qml;
-        "components/WifiIndicator.qml" = fileText ./components/WifiIndicator.qml;
-        "components/Tray.qml" = fileText ./components/Tray.qml;
-      };
-
-      # HM module provides the user unit; hook it to a reasonable target
       systemd.target = "graphical-session.target";
     };
 
-    # If you want to disable autostart, we simply don't enable the unit via HM:
-    # (Some HM versions do this via systemd.enable; if your HM doesn't support it,
-    # you can just set autostart=false and manually run quickshell.)
+    # Optional: if you want to “disable autostart” later, we’d override systemd
     assertions = lib.optional (!cfg.autostart) {
       assertion = true;
       message = "autostart=false: start quickshell manually with `quickshell -c ${configName}`.";
