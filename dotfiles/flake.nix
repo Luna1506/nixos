2327e9cb606d40386a4cad7f95ea7e8b7b376432
Luna <mhaiplick1506@gmail.com>
2026-03-12 21:07:58 +0100

    2026-03-12 21:07:58

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -21,48 +21,48 @@
 
     hyprfrost = {
       url = "path:../../projects/hyprfrost/";
-    }
-      };
+    };
+  };
 
-    outputs = inputs@{ self, nixpkgs, home-manager, ... }:
-      let
-        system = "x86_64-linux";
-        username = "luna";
-        fullname = "Luna Haiplick";
-        nvidiaAlternative = true;
-        monitor = "eDP-1";
-        zoom = "1";
-        git-name = "Luna";
-        git-email = "mhaiplick1506@gmail.com";
-        luna-path = true;
-      in
-      {
-        # --- NixOS wie bisher ---
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-          inherit system;
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      username = "luna";
+      fullname = "Luna Haiplick";
+      nvidiaAlternative = true;
+      monitor = "eDP-1";
+      zoom = "1";
+      git-name = "Luna";
+      git-email = "mhaiplick1506@gmail.com";
+      luna-path = true;
+    in
+    {
+      # --- NixOS wie bisher ---
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
 
-          specialArgs = {
-            inherit inputs username fullname nvidiaAlternative monitor zoom git-name git-email luna-path;
-          };
-
-          modules = [
-            ./hosts/laptop/default.nix
-
-            home-manager.nixosModules.home-manager
-
-            {
-              home-manager.useUserPackages = true;
-
-              home-manager.users.${username} =
-                import ./home/${username}/home.nix;
-
-              home-manager.extraSpecialArgs = {
-                inherit inputs username fullname monitor zoom git-name git-email;
-              };
-
-              home-manager.backupFileExtension = "backup";
-            }
-          ];
+        specialArgs = {
+          inherit inputs username fullname nvidiaAlternative monitor zoom git-name git-email luna-path;
         };
+
+        modules = [
+          ./hosts/laptop/default.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useUserPackages = true;
+
+            home-manager.users.${username} =
+              import ./home/${username}/home.nix;
+
+            home-manager.extraSpecialArgs = {
+              inherit inputs username fullname monitor zoom git-name git-email;
+            };
+
+            home-manager.backupFileExtension = "backup";
+          }
+        ];
       };
-  }
+    };
+}
